e73253ee775a21c3c1ee4d4af95fb43be3f62bb5
amit03gh-dev <amit03gh@gmail.com>
2025-10-11 15:43:36 +0530

    performance testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,17 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* Preconnect for faster font loading */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        
+        {/* Load Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body  className={`${inter.variable} font-sans`} data-mobile-nav-style="classic">
         <Navbar data = {services.data}/>
         <BootstrapProvider>
